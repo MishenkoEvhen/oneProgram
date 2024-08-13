c6d588c60ec584f46e76a3101a6cfd67ea609a72
MishenkoEvhen <111456305+MishenkoEvhen@users.noreply.github.com>
2024-08-13 18:27:21 +0300

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double numOne = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double numTwo = Convert.ToDouble(Console.ReadLine());
+
+            double result = numOne + numTwo;
+
+            Console.WriteLine("Вот результат сложения двух чисел: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
